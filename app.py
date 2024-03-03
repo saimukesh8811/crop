c9d538b68ff65a81b4e0fe2e80f3dd3fccdad5ce
GOLLA-SAIRAM <147690746+GOLLA-SAIRAM@users.noreply.github.com>
2024-03-03 22:18:33 +0530

    app.py

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def predict():
         return render_template('index.html', recommended_crop=recommended_crop)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
